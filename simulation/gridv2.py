12720818cd0d12e0d048630a0640e490a88f1330
Louis <louis.lesueur@gmx.fr>
2020-09-23 00:55:47 +0200

    everything working

--- simulation/gridv2.py
+++ simulation/gridv2.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 
+def grad(D, axis=0):
+    D_pad = np.pad(D, (1,1), 'reflect')
+    return (np.roll(D_pad,1,axis=axis) - D_pad)[1:-1,1:-1]
+
 def grad_grad(D, A):
     A_pad = np.pad(A, (1,1), 'reflect')
     D_pad = np.pad(D, (1,1), 'reflect')
@@ -19,57 +23,59 @@ def lap(A):
     A_pad = np.pad(A, (1,1), 'reflect')
     return ((np.roll(A_pad,-1,axis=1) - 2*A_pad + np.roll(A_pad,1,axis=1)) + (np.roll(A_pad,-1,axis=0) - 2*A_pad + np.roll(A_pad,1,axis=0)))[1:-1,1:-1]
 
+
+
 class Grid:
-    def __init__(self,R0, N0, alpha, w, c, gamma, a, KN, r, KR, bound):
-        self.R = R0
-        self.N = N0
+    def __init__(self, rho_0, pi_0, alpha, w, c, gamma, a, Kpi, r, Krho, bound):
+        self.rho = rho_0
+        self.pi = pi_0
         self.alpha = alpha
         self.w = w
         self.c = c
         self.gamma = gamma
-        self.a = alpha
-        self.KN = KN
+        self.a = a
+        self.Kpi = Kpi
         self.r = r
-        self.KR = KR
+        self.Krho = Krho
         self.bound = bound
+        self.area = np.sum(bound)
+
+        self.repro = np.zeros_like(self.pi)
 
     def update(self):
 
-        def DN(N, K):
-            out = np.sum([Rr for Rr in self.R], axis=0)
-            out[np.where(2*N/K > 1)] = 0
-            for i in range(self.R.shape[0]):
-                out[np.where(2*self.R[i]/self.KR[i]<1)]=0
-            return out
+        def DN(r, rr):
+            res = np.sum(ro for ro in self.rho)
+            return (res/np.linalg.norm(res))*np.exp(-r)
 
-        for i in range(self.N.shape[0]):
-            conso = self.c[i]*np.sum([self.w[j]*self.a[i,j]*self.R[j] for j in range(self.R.shape[0])], axis=0)
+        for i in range(self.pi.shape[0]):
+            conso = self.c[i]*np.sum([self.w[j]*self.a[i,j]*self.rho[j] for j in range(self.rho.shape[0])], axis=0)
             death = -self.gamma[i]
-            war = - np.sum([self.alpha[i,k]*self.N[k] for k in range(self.N.shape[0])], axis=0)/self.KN[i]
+            war = -np.sum([self.alpha[i,k]*self.pi[k] for k in range(self.pi.shape[0])], axis=0)/self.Kpi[i]
 
-            migration =  grad_grad(DN(self.N[i], self.KN[i]), self.N[i]) + DN(self.N[i], self.KN[i])*lap(self.N[i])
-            #migration = 0.00005*DN(self.N[i], self.KN[i])*lap(self.N[i])
+            self.repro[i] = conso+death+war
 
-            self.N[i] += self.N[i]*(conso+death+war) + migration
-            self.N[i] *= self.bound
+            migration =  grad_grad(DN(self.repro[i], np.sum(ro for ro in self.rho)),self.pi[i]) + DN(self.repro[i], np.sum(ro for ro in self.rho))*lap(self.pi[i])
+            #migration = DN(self.pi[i], self.Kpi[i])*lap(self.pi[i])
 
-        def DR(R, K):
-            out = np.sum([Nr for Nr in self.N], axis=0)
-            out[np.where(2*R/K < 1)] = 0
-            for i in range(self.N.shape[0]):
-                out[np.where(2*self.N[i]/self.KN[i]>1)]=0
-            return out
+            self.pi[i] += self.pi[i]*self.repro[i] + migration
+            self.pi[i] *= self.bound
 
-        for j in range(self.R.shape[0]):
+        def DR(r):
+            return 0.02*np.exp(0.01*r)
+
+        for j in range(self.rho.shape[0]):
             renew = self.r[j]
-            thresh = -(self.r[j]*self.R[j])/self.KR[j]
-            conso = -np.sum([self.a[k,j]*self.N[k] for k in range(self.N.shape[0])], axis=0)
+            thresh = -(self.r[j]*self.rho[j])/self.Krho[j]
+            conso = -np.sum([self.a[k,j]*self.pi[k] for k in range(self.pi.shape[0])], axis=0)
 
-            exchange = grad_grad(DR(self.R[j], self.KR[j]), self.R[j]) + DR(self.R[j], self.KR[j])*lap(self.R[j])
-            #exchange = 0.00005*DR(self.R[j], self.KR[j])*lap(self.R[j])
+            repro_res = (renew+thresh+conso)
+            exchange = np.sum([grad_grad(DR(self.repro[i]), self.rho[j]) + DR(self.repro[i])*lap(self.rho[j]) for i in range(self.pi.shape[0])], axis=0)
+            #exchange = np.sum(DR(self), self.rho[j])*lap(self.rho[j]), axis=0)
+            #exchange = 0
 
-            self.R[j] += self.R[j]*(renew+thresh+conso) + exchange
-            self.R[j] *= self.bound
+            self.rho[j] += self.rho[j]*repro_res + exchange
+            self.rho[j] *= self.bound
 
 
 
@@ -79,7 +85,7 @@ I_R0 = plt.imread('maps/europe_r.png')
 
 I = 0.2989 * I[:,:,0] + 0.5870 * I[:,:,1] + 0.1140 * I[:,:,2]
 I_r = (0.2989 * I_r[:,:,0] + 0.5870 * I_r[:,:,1] + 0.1140 * I_r[:,:,2])*0.08
-I_R0 = (0.2989 * I_R0[:,:,0] + 0.5870 * I_R0[:,:,1] + 0.1140 * I_R0[:,:,2])*1250+1
+I_R0 = (0.2989 * I_R0[:,:,0] + 0.5870 * I_R0[:,:,1] + 0.1140 * I_R0[:,:,2])*100+1
 
 N1 = np.zeros_like(I)
 N2 = np.zeros_like(I)
@@ -93,13 +99,13 @@ sim = Grid(np.array([I_R0]), #R
            np.array([[0,0.04],  #alpha
                      [0.03,0]]),
            np.array([1]), #w
-           np.array([0.01, 0.01]), #c
-           np.array([0.05*np.ones(N1.shape), 0.05*np.ones(N2.shape)]), #gamma
-           np.array([[1],
-                     [1]]), #a
-           np.array([1000, 400]), #KN
+           np.array([0.2, 0.1]), #c
+           np.array([0.01*np.ones(N1.shape), 0.05*np.ones(N2.shape)]), #gamma
+           np.array([[0.005],
+                     [0.01]]), #a
+           np.array([30, 40]), #KN
            np.array([I_r]), #r
-           np.array([I_R0]),
+           np.array([2*I_R0]), #KR
            I) #KR
 
 #Tmax = 1500
@@ -122,19 +128,25 @@ sim = Grid(np.array([I_R0]), #R
 
 def colormap(I, N, colors):
 
-    N_out = np.zeros((N.shape[0], N.shape[1], N.shape[2], 3))
+    N_out = I.copy()
     for i in range(len(N)):
-        N_out[i] = I
-        N_out[i][np.where(N[i]>0)] = colors[0]
+        for k in range(N[i].shape[0]):
+            for l in range(N[i].shape[1]):
+                if N[i][k,l] > 0:
+                    N_out[k,l] += colors[i]
+    return N_out[:,:,:3]
 
+
+I = plt.imread('maps/europe.png')
+print(I)
 
 fig = plt.figure()
 ax = plt.axes()
-im=plt.imshow(sim.N[0]+sim.N[1],interpolation='none')
+im=plt.imshow(sim.pi[0]+sim.pi[1])
 
 def animate(i):
     sim.update()
-    im.set_array(sim.N[0]+sim.N[1])
+    im.set_array(sim.pi[0]+sim.pi[1])
     return [im]
 
 writer = PillowWriter(fps=25)
