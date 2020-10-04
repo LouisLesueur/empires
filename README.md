fa84156b8101dd1e13d090ca97cddafd9fa38ae4
Louis <louis.lesueur@gmx.fr>
2020-10-04 10:25:23 +0200

    clear readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# empires
+# Empires
+
+A simulation of human interactions at a large scale.
+It is a simple reaction–diffusion system based on darwinian dynamics.
+
+# Sources
+
+On taxis and kinesis equations:
++ Arthur, R. M. « SPECIES PACKING, AND WHAT COMPETITION MINIMIZES ». _Proceedings of the National Academy of Sciences_, vol. 64, no 4, décembre 1969, p. 1369‑71. DOI.org (Crossref), doi:10.1073/pnas.64.4.1369.
++ Averill, Isabel, et al. « On Several Conjectures from Evolution of Dispersal ». _Journal of Biological Dynamics_, vol. 6, no 2, mars 2012, p. 117‑30. DOI.org (Crossref), doi:10.1080/17513758.2010.529169.
++ Gorban, A. N., et N. Çabukoǧlu. « Basic Model of Purposeful Kinesis ». _Ecological Complexity_, vol. 33, janvier 2018, p. 75‑83. DOI.org (Crossref), doi:10.1016/j.ecocom.2018.01.002.
++ Hillen, T., et K. J. Painter. « A User’s Guide to PDE Models for Chemotaxis ». _Journal of Mathematical Biology_, vol. 58, no 1‑2, janvier 2009, p. 183‑217. DOI.org (Crossref), doi:10.1007/s00285-008-0201-3.
+
+
+On evolutionary game theory:
++ Tembine, Hamidou, et al. « Evolutionary Games in Wireless Networks ». _IEEE Transactions on Systems, Man, and Cybernetics, Part B (Cybernetics)_, vol. 40, no 3, juin 2010, p. 634‑46. DOI.org (Crossref), doi:10.1109/TSMCB.2009.2034631.
++ Cressman R., Apaloo J. (2016) Evolutionary Game Theory. In: Basar T., Zaccour G. (eds) Handbook of Dynamic Game Theory. Springer, Cham. https://doi.org/10.1007/978-3-319-27335-8_6-1
++ Perc, Matjaž, et al. « Statistical Physics of Human Cooperation ». _Physics Reports_, vol. 687, mai 2017, p. 1‑51. DOI.org (Crossref), doi:10.1016/j.physrep.2017.05.004.
++ Sandholm, William H. « Evolutionary Game Theory ». _Encyclopedia of Complexity and Systems Science_, édité par Robert A. Meyers, Springer Berlin Heidelberg, 2017, p. 1‑38. DOI.org (Crossref), doi:10.1007/978-3-642-27737-5_188-3.
--- main.py
+++ main.py
@@ -15,13 +15,42 @@ N = 10
 PI = []
 
 count = 0
-while(count) < N:
-    i = np.random.randint(10, Europe.shape[0]-10)
-    j = np.random.randint(10, Europe.shape[1]-10)
+while(count) < N//4:
+    i = np.random.randint(10, Europe.shape[0]//2-10)
+    j = np.random.randint(10, Europe.shape[1]//2-10)
 
     if Europe.I[i,j] != 0:
         count +=1
         PI.append(Pop([i, j], 200, np.random.randint(255, size=3), 0.001, 0.05, 1000, Europe.shape, Europe.area))
+
+count = 0
+while(count) < N//4:
+    i = np.random.randint(Europe.shape[0]//2-10, Europe.shape[0]-10)
+    j = np.random.randint(Europe.shape[0]//2-10, Europe.shape[1]-10)
+
+    if Europe.I[i,j] != 0:
+        count +=1
+        PI.append(Pop([i, j], 200, np.random.randint(255, size=3), 0.001, 0.05, 1000, Europe.shape, Europe.area))
+
+count = 0
+while(count) < N//4:
+    i = np.random.randint(Europe.shape[0]//2-10, Europe.shape[0]-10)
+    j = np.random.randint(10, Europe.shape[1]//2-10)
+
+    if Europe.I[i,j] != 0:
+        count +=1
+        PI.append(Pop([i, j], 200, np.random.randint(255, size=3), 0.001, 0.05, 1000, Europe.shape, Europe.area))
+
+count = 0
+while(count) < N//4:
+    i = np.random.randint(10, Europe.shape[0]//2-10)
+    j = np.random.randint(Europe.shape[0]//2-10, Europe.shape[1]-10)
+
+    if Europe.I[i,j] != 0:
+        count +=1
+        PI.append(Pop([i, j], 200, np.random.randint(255, size=3), 0.001, 0.05, 1000, Europe.shape, Europe.area))
+
+
 
 Rome =  Pop([1189, 714], 200, [1,0,0], 0.001, 0.05, 1000, Europe.shape, Europe.area)
 Gaule = Pop([929, 493],  200, [0,1,0], 0.001, 0.05, 1000, Europe.shape, Europe.area)
@@ -33,7 +62,7 @@ RHO = np.array([Miam])
 
 
 
-sim = Grid(np.array(PI), RHO, Europe, 40)
+sim = Grid(np.array(PI), RHO, Europe, 10)
 
 
 
@@ -52,6 +81,6 @@ def animate(i):
     return [im]
 
 writer = PillowWriter(fps=15)
-anim = FuncAnimation(fig, animate, frames = 150, interval = 50)
-#anim.save('out.gif', writer=writer)
+anim = FuncAnimation(fig, animate, frames = 500, interval = 50)
+anim.save('out.gif', writer=writer)
 plt.show()
