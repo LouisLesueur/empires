correct derivative

grid/grid.py
@@ -51,13 +51,13 @@ class Grid:
 
         for i,pi in enumerate(self.PI):
 
-            pi.repro = pi.G(U,self.RHO,self.PI, i)
+            pi.repro = pi.G(U,self.RHO,self.PI)
             migration = pi.DN(pi.repro)*lap(pi.pi, pi.dx)
             shift = pi.drift0*pi.pi*lap(np.sum([rho.rho for rho in self.RHO], axis=0), pi.dx)
 
             pi.pi += pi.pi*pi.repro + migration + shift
 
-            pi.v += np.sum([((pi.v-U[i])/2)*pi.alpha(U[i]) for i in range(self.PI.shape[0])])/(pi.KN+1e-6)
+            pi.v += pi.partG(U,self.RHO,self.PI)
 
         for j,rho in enumerate(self.RHO):
             renew = rho.r

maps/domain.py
@@ -135,11 +135,14 @@ class Pop:
         return np.exp(-(self.v-u)**2/4)
 
 
-    def G(self, U, RHO, PI, i):
+    def G(self, U, RHO, PI):
         conso = self.c*np.sum([RHO[j].prop*RHO[j].rho for j in range(RHO.shape[0])], axis=0)
         death = -self.gamma
         war = -self.gamma*np.sum([self.alpha(U[i])*PI[i].pi for i in range(PI.shape[0])], axis=0)/(self.KN+1e-6)
         return conso+death+war
+
+    def partG(self, U, RHO, PI):
+        return self.gamma*np.sum([((self.v-U[i])/2)*self.alpha(U[i])*PI[i].pi for i in range(PI.shape[0])])/(self.KN+1e-6)
 
     def DN(self, r):
         """Diffusion coefficient (see Gorban and all)"""